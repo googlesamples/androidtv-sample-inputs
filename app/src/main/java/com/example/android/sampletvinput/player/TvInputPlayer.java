52430a77030e18fc36d1bdcca974bb031e8f6b54
Dongwon Kang <dwkang@google.com>
2015-01-27 18:09:17 +0900

    Implement MPEG_DASH and multi-audio support.
    Change-Id: I781f1910fe1ee7f2e747f283c1f16424554d3717

--- app/src/main/java/com/example/android/sampletvinput/player/TvInputPlayer.java
+++ app/src/main/java/com/example/android/sampletvinput/player/TvInputPlayer.java
@@ -10,26 +10,46 @@ import android.os.Build;
 import android.os.Handler;
 import android.view.Surface;
 
+import com.google.android.exoplayer.DefaultLoadControl;
 import com.google.android.exoplayer.DummyTrackRenderer;
 import com.google.android.exoplayer.ExoPlaybackException;
 import com.google.android.exoplayer.ExoPlayer;
 import com.google.android.exoplayer.ExoPlayerLibraryInfo;
 import com.google.android.exoplayer.FrameworkSampleSource;
+import com.google.android.exoplayer.LoadControl;
 import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
+import com.google.android.exoplayer.MediaCodecUtil;
 import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.SampleSource;
 import com.google.android.exoplayer.TrackRenderer;
+import com.google.android.exoplayer.chunk.ChunkSampleSource;
+import com.google.android.exoplayer.chunk.ChunkSource;
+import com.google.android.exoplayer.chunk.Format;
+import com.google.android.exoplayer.chunk.FormatEvaluator;
+import com.google.android.exoplayer.chunk.MultiTrackChunkSource;
+import com.google.android.exoplayer.dash.DashChunkSource;
+import com.google.android.exoplayer.dash.mpd.AdaptationSet;
+import com.google.android.exoplayer.dash.mpd.MediaPresentationDescription;
+import com.google.android.exoplayer.dash.mpd.MediaPresentationDescriptionParser;
+import com.google.android.exoplayer.dash.mpd.Period;
+import com.google.android.exoplayer.dash.mpd.Representation;
 import com.google.android.exoplayer.hls.HlsChunkSource;
 import com.google.android.exoplayer.hls.HlsPlaylist;
 import com.google.android.exoplayer.hls.HlsPlaylistParser;
 import com.google.android.exoplayer.hls.HlsSampleSource;
 import com.google.android.exoplayer.text.TextRenderer;
 import com.google.android.exoplayer.text.eia608.Eia608TrackRenderer;
+import com.google.android.exoplayer.upstream.BufferPool;
 import com.google.android.exoplayer.upstream.DataSource;
 import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer.upstream.UriDataSource;
 import com.google.android.exoplayer.util.ManifestFetcher;
+import com.google.android.exoplayer.util.MimeTypes;
+import com.google.android.exoplayer.util.Util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -54,15 +74,21 @@ public class TvInputPlayer implements TextRenderer {
     private static final int MIN_BUFFER_MS = 1000;
     private static final int MIN_REBUFFER_MS = 5000;
 
+    private static final int BUFFER_SEGMENT_SIZE = 64 * 1024;
+    private static final int VIDEO_BUFFER_SEGMENTS = 200;
+    private static final int AUDIO_BUFFER_SEGMENTS = 60;
+    private static final int LIVE_EDGE_LATENCY_MS = 30000;
+
     private final Handler mHandler;
     private final ExoPlayer mPlayer;
-    private MediaCodecVideoTrackRenderer mVideoRenderer;
-    private MediaCodecAudioTrackRenderer mAudioRenderer;
+    private TrackRenderer mVideoRenderer;
+    private TrackRenderer mAudioRenderer;
     private TrackRenderer mTextRenderer;
     private final CopyOnWriteArrayList<Callback> mCallbacks;
     private float mVolume;
     private Surface mSurface;
-    private TvTrackInfo[][] mTvTracks = new TvTrackInfo[3][];
+    private TvTrackInfo[][] mTvTracks = new TvTrackInfo[RENDERER_COUNT][];
+    private MultiTrackChunkSource[] mMultiTrackSources = new MultiTrackChunkSource[RENDERER_COUNT];
 
     public TvInputPlayer() {
         mHandler = new Handler();
@@ -135,8 +161,7 @@ public class TvInputPlayer implements TextRenderer {
                             // TODO: Implement custom HLS source to get the internal track metadata.
                             mTvTracks[TvTrackInfo.TYPE_SUBTITLE] = new TvTrackInfo[1];
                             mTvTracks[TvTrackInfo.TYPE_SUBTITLE][0] =
-                                    new TvTrackInfo.Builder(TvTrackInfo.TYPE_SUBTITLE, "text1")
-                                        .setLanguage("eng")
+                                    new TvTrackInfo.Builder(TvTrackInfo.TYPE_SUBTITLE, "1")
                                         .build();
                             prepareInternal();
                         }
@@ -148,8 +173,124 @@ public class TvInputPlayer implements TextRenderer {
                             }
                         }
                     });
+        } else if (sourceType == SOURCE_TYPE_MPEG_DASH) {
+            final String userAgent = getUserAgent(context);
+            MediaPresentationDescriptionParser parser = new MediaPresentationDescriptionParser();
+            final ManifestFetcher<MediaPresentationDescription> manifestFetcher =
+                    new ManifestFetcher<>(parser, uri.toString(), uri.toString(), userAgent);
+            manifestFetcher.singleLoad(mHandler.getLooper(),
+                    new ManifestFetcher.ManifestCallback<MediaPresentationDescription>() {
+                @Override
+                public void onManifest(String contentId, MediaPresentationDescription manifest) {
+                    Period period = manifest.periods.get(0);
+                    LoadControl loadControl = new DefaultLoadControl(new BufferPool(
+                            BUFFER_SEGMENT_SIZE));
+                    DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+
+                    // Determine which video representations we should use for playback.
+                    int maxDecodableFrameSize;
+                    try {
+                        maxDecodableFrameSize = MediaCodecUtil.maxH264DecodableFrameSize();
+                    } catch (MediaCodecUtil.DecoderQueryException e) {
+                        for (Callback callback : mCallbacks) {
+                            callback.onPlayerError(new ExoPlaybackException(e));
+                        }
+                        return;
+                    }
+
+                    int videoAdaptationSetIndex = period.getAdaptationSetIndex(
+                            AdaptationSet.TYPE_VIDEO);
+                    List<Representation> videoRepresentations =
+                            period.adaptationSets.get(videoAdaptationSetIndex).representations;
+                    ArrayList<Integer> videoRepresentationIndexList = new ArrayList<Integer>();
+                    for (int i = 0; i < videoRepresentations.size(); i++) {
+                        Format format = videoRepresentations.get(i).format;
+                        if (format.width * format.height > maxDecodableFrameSize) {
+                            // Filtering stream that device cannot play
+                        } else if (!format.mimeType.equals(MimeTypes.VIDEO_MP4)
+                                && !format.mimeType.equals(MimeTypes.VIDEO_WEBM)) {
+                            // Filtering unsupported mime type
+                        } else {
+                            videoRepresentationIndexList.add(i);
+                        }
+                    }
+
+                    // Build the video renderer.
+                    if (videoRepresentationIndexList.isEmpty()) {
+                        mVideoRenderer = new DummyTrackRenderer();
+                    } else {
+                        int[] videoRepresentationIndices = Util.toArray(
+                                videoRepresentationIndexList);
+                        DataSource videoDataSource = new UriDataSource(userAgent, bandwidthMeter);
+                        ChunkSource videoChunkSource = new DashChunkSource(manifestFetcher,
+                                videoAdaptationSetIndex, videoRepresentationIndices,
+                                videoDataSource,
+                                new FormatEvaluator.AdaptiveEvaluator(bandwidthMeter),
+                                LIVE_EDGE_LATENCY_MS);
+                        ChunkSampleSource videoSampleSource = new ChunkSampleSource(
+                                videoChunkSource, loadControl,
+                                VIDEO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, true);
+                        mVideoRenderer = new MediaCodecVideoTrackRenderer(videoSampleSource,
+                                MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT, 0, mHandler, null,
+                                50);
+                    }
+
+                    // Build the audio chunk sources.
+                    int audioAdaptationSetIndex = period.getAdaptationSetIndex(
+                            AdaptationSet.TYPE_AUDIO);
+                    AdaptationSet audioAdaptationSet = period.adaptationSets.get(
+                            audioAdaptationSetIndex);
+                    List<ChunkSource> audioChunkSourceList = new ArrayList<ChunkSource>();
+                    List<TvTrackInfo> audioTrackList = new ArrayList<>();
+                    if (audioAdaptationSet != null) {
+                        DataSource audioDataSource = new UriDataSource(userAgent, bandwidthMeter);
+                        FormatEvaluator audioEvaluator = new FormatEvaluator.FixedEvaluator();
+                        List<Representation> audioRepresentations =
+                                audioAdaptationSet.representations;
+                        for (int i = 0; i < audioRepresentations.size(); i++) {
+                            Format format = audioRepresentations.get(i).format;
+                            audioTrackList.add(new TvTrackInfo.Builder(TvTrackInfo.TYPE_AUDIO,
+                                    Integer.toString(i))
+                                    .setAudioChannelCount(format.numChannels)
+                                    .setAudioSampleRate(format.audioSamplingRate)
+                                    .setLanguage(format.language)
+                                    .build());
+                            audioChunkSourceList.add(new DashChunkSource(manifestFetcher,
+                                    audioAdaptationSetIndex, new int[] {i}, audioDataSource,
+                                    audioEvaluator, LIVE_EDGE_LATENCY_MS));
+                        }
+                    }
+
+                    // Build the audio renderer.
+                    final MultiTrackChunkSource audioChunkSource;
+                    if (audioChunkSourceList.isEmpty()) {
+                        mAudioRenderer = new DummyTrackRenderer();
+                    } else {
+                        audioChunkSource = new MultiTrackChunkSource(audioChunkSourceList);
+                        SampleSource audioSampleSource = new ChunkSampleSource(audioChunkSource,
+                                loadControl, AUDIO_BUFFER_SEGMENTS * BUFFER_SEGMENT_SIZE, true);
+                        mAudioRenderer = new MediaCodecAudioTrackRenderer(audioSampleSource);
+                        TvTrackInfo[] tracks = new TvTrackInfo[audioTrackList.size()];
+                        audioTrackList.toArray(tracks);
+                        mTvTracks[TvTrackInfo.TYPE_AUDIO] = tracks;
+                        mMultiTrackSources[TvTrackInfo.TYPE_AUDIO] = audioChunkSource;
+                    }
+
+                    // Build the text renderer.
+                    mTextRenderer = new DummyTrackRenderer();
+
+                    prepareInternal();
+                }
+
+                @Override
+                public void onManifestError(String contentId, IOException e) {
+                    for (Callback callback : mCallbacks) {
+                        callback.onPlayerError(new ExoPlaybackException(e));
+                    }
+                }
+            });
         } else {
-            // TODO: handle DASH and SS.
+            // TODO: handle SS case.
         }
     }
 
@@ -161,12 +302,26 @@ public class TvInputPlayer implements TextRenderer {
     }
 
     public boolean selectTrack(int trackType, String trackId) {
-        if (trackType == TvTrackInfo.TYPE_SUBTITLE) {
-            // TODO: Handle the case for multiple tracks.
-            mPlayer.setRendererEnabled(trackType, trackId != null);
-            return true;
+        if (trackType < 0 || trackType >= mTvTracks.length) {
+            return false;
         }
-        return false;
+        if (trackId == null) {
+            mPlayer.setRendererEnabled(trackType, false);
+        } else {
+            int trackIndex = Integer.parseInt(trackId);
+            if (mMultiTrackSources[trackType] == null) {
+                mPlayer.setRendererEnabled(trackType, true);
+            } else {
+                boolean playWhenReady = mPlayer.getPlayWhenReady();
+                mPlayer.setPlayWhenReady(false);
+                mPlayer.setRendererEnabled(trackType, false);
+                mPlayer.sendMessage(mMultiTrackSources[trackType],
+                        MultiTrackChunkSource.MSG_SELECT_TRACK, trackIndex);
+                mPlayer.setRendererEnabled(trackType, true);
+                mPlayer.setPlayWhenReady(playWhenReady);
+            }
+        }
+        return true;
     }
 
     public void setPlayWhenReady(boolean playWhenReady) {
