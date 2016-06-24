b259889f157daef0ad2b3eb9cc4750caa2655baa
Guangyu Chen <guangyuc@google.com>
2016-06-24 13:34:08 -0700

    Integrate IMA SDK to support linear VAST video ads insertion.
    Sample input will insert ads when users tuned to a channel if the
    channel has ads information.
    
    Bug: 28941560
    Change-Id: I62fad9388ffdd157141504092a26d64341e948c0

---
+++ app/src/main/java/com/example/android/sampletvinput/ads/AdVideoPlayerProxy.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.sampletvinput.ads;
+
+import com.example.android.sampletvinput.player.DemoPlayer;
+import com.google.android.exoplayer.ExoPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An implementation of {@link AdController.VideoPlayer} with {@link DemoPlayer}.
+ */
+public class AdVideoPlayerProxy implements AdController.VideoPlayer {
+    private final DemoPlayer mDemoPlayer;
+    private final List<PlayerCallback> mVideoPlayerCallbacks;
+
+    public AdVideoPlayerProxy(DemoPlayer demoPlayer) {
+        mDemoPlayer = demoPlayer;
+        mDemoPlayer.addListener(new DemoPlayer.Listener() {
+            @Override
+            public void onStateChanged(boolean playWhenReady, int playbackState) {
+                if (playWhenReady && playbackState == ExoPlayer.STATE_ENDED) {
+                    for (PlayerCallback callback : mVideoPlayerCallbacks) {
+                        callback.onCompleted();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                // Do nothing.
+            }
+
+            @Override
+            public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees,
+                    float pixelWidthHeightRatio) {
+                // Do nothing.
+            }
+        });
+        mVideoPlayerCallbacks = new ArrayList<>();
+    }
+
+    @Override
+    public void play() {
+        mDemoPlayer.setPlayWhenReady(true);
+        for (PlayerCallback callback : mVideoPlayerCallbacks) {
+            callback.onPlay();
+        }
+    }
+
+    @Override
+    public void pause() {
+        mDemoPlayer.setPlayWhenReady(false);
+        for (PlayerCallback callback : mVideoPlayerCallbacks) {
+            callback.onPause();
+        }
+    }
+
+    @Override
+    public void stop() {
+        mDemoPlayer.setPlayWhenReady(false);
+    }
+
+    @Override
+    public void seekTo(long videoPositionMs) {
+        mDemoPlayer.seekTo(videoPositionMs);
+    }
+
+    @Override
+    public long getCurrentPosition() {
+        return mDemoPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public long getDuration() {
+        return mDemoPlayer.getDuration();
+    }
+
+    @Override
+    public void addPlayerCallback(PlayerCallback callback) {
+        mVideoPlayerCallbacks.add(callback);
+    }
+
+    @Override
+    public void removePlayerCallback(PlayerCallback callback) {
+        mVideoPlayerCallbacks.remove(callback);
+    }
+}
---
+++ library/src/main/java/com/example/android/sampletvinput/ads/AdController.java
@@ -0,0 +1,358 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.sampletvinput.ads;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.ads.interactivemedia.v3.api.AdDisplayContainer;
+import com.google.ads.interactivemedia.v3.api.AdErrorEvent;
+import com.google.ads.interactivemedia.v3.api.AdEvent;
+import com.google.ads.interactivemedia.v3.api.AdsLoader;
+import com.google.ads.interactivemedia.v3.api.AdsManager;
+import com.google.ads.interactivemedia.v3.api.AdsManagerLoadedEvent;
+import com.google.ads.interactivemedia.v3.api.AdsRequest;
+import com.google.ads.interactivemedia.v3.api.ImaSdkFactory;
+import com.google.ads.interactivemedia.v3.api.player.VideoAdPlayer;
+import com.google.ads.interactivemedia.v3.api.player.VideoProgressUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A controller class integrated with <a href="https://developers.google.com/interactive-media-ads/docs/sdks/android/">Google IMA SDK</a>,
+ * which can be used to request <a href="http://www.iab.com/guidelines/digital-video-ad-serving-template-vast-3-0/">VAST</a>
+ * video ads, handle ad error and track the progress of ad playback.
+ */
+public class AdController implements AdErrorEvent.AdErrorListener,
+        AdsLoader.AdsLoadedListener, AdEvent.AdEventListener {
+    private static final String TAG = "AdController";
+    private static final boolean DEBUG = false;
+
+    /**
+     * Callback interface used to delegate major ad request events.
+     */
+    public interface AdControllerCallback {
+        /**
+         * This is called when the advertisement request finished and advertisement video is about
+         * to be played.
+         *
+         * @param adVideoUrl URL of advertisement video.
+         * @return A {@link VideoPlayer} instance constructed by
+         * {@link android.media.tv.TvInputService}
+         */
+        VideoPlayer onAdReadyToPlay(String adVideoUrl);
+
+        /**
+         * This is called when advertisement has successfully finished its video playback. This
+         * method should be implemented to resume channel content after ad playback.
+         */
+        void onAdCompleted();
+
+        /**
+         * This is called when advertisement error occurs. Implementing this method ensures that
+         * {@link android.media.tv.TvInputService} will not get stuck in ad request forever.
+         */
+        void onAdError();
+    }
+
+    /**
+     * Interface definition for controlling video playback.
+     */
+    public interface VideoPlayer {
+
+        /**
+         *  Interface for alerting caller of major video events.
+         */
+        public interface PlayerCallback {
+
+            /**
+             * Called when the current video starts playing from the beginning.
+             */
+            void onPlay();
+
+            /**
+             * Called when the current video has completed playback to the end of the video.
+             */
+            void onCompleted();
+
+            /**
+             * Called when an error occurs during video playback.
+             */
+            void onError();
+
+            /**
+             * Called when the video is paused.
+             */
+            void onPause();
+
+            /**
+             * Called when the video is resumed.
+             */
+            void onResume();
+        }
+
+        /**
+         * Play the currently loaded video from its current position.
+         */
+        void play();
+
+        /**
+         * Pause the currently loaded video.
+         */
+        void pause();
+
+        /**
+         * Stop playing the currently loaded video.
+         */
+        void stop();
+
+        /**
+         * Progress the currently loaded video to the given position (milliseconds).
+         */
+        void seekTo(long videoPositionMs);
+
+        /**
+         * Get the playback progress state (milliseconds) of the current video.
+         */
+        long getCurrentPosition();
+
+        /**
+         * Get the total length of the currently loaded video in milliseconds.
+         */
+        long getDuration();
+
+        /**
+         * Provide the player with a callback for major video events (pause, complete, resume, etc).
+         */
+        void addPlayerCallback(PlayerCallback callback);
+
+        /**
+         * Remove a player callback from getting notified on video events.
+         */
+        void removePlayerCallback(PlayerCallback callback);
+    }
+
+    // Container with references to video player and ad UI ViewGroup.
+    private AdDisplayContainer mAdDisplayContainer;
+
+    // The AdsLoader instance exposes the requestAds method.
+    private AdsLoader mAdsLoader;
+
+    // AdsManager exposes methods to control ad playback and listen to ad events.
+    private AdsManager mAdsManager;
+
+    // Factory class for creating SDK objects.
+    private ImaSdkFactory mSdkFactory;
+
+    // IMA SDK requires a UI to be passed in, although TV input service does not need a UI
+    // component.
+    private ViewGroup mStubViewGroup;
+
+    // Callback used to define behavior of the ad.
+    private AdControllerCallback mAdControllerCallback;
+
+    public AdController(Context context) {
+        // Create an AdsLoader.
+        mSdkFactory = ImaSdkFactory.getInstance();
+        mAdsLoader = mSdkFactory.createAdsLoader(context);
+        mAdsLoader.addAdErrorListener(this);
+        mAdsLoader.addAdsLoadedListener(this);
+
+        mStubViewGroup = new FrameLayout(context);
+    }
+
+    /**
+     * Requests video ads from the given VAST ad tag.
+     *
+     * @param adRequestUrl URL of the ad's VAST XML.
+     */
+    public void requestAds(@NonNull String adRequestUrl,
+           @NonNull AdControllerCallback adControllerCallback) {
+        mAdControllerCallback = adControllerCallback;
+
+        mAdDisplayContainer = mSdkFactory.createAdDisplayContainer();
+        mAdDisplayContainer.setPlayer(new VideoAdPlayerImpl());
+        mAdDisplayContainer.setAdContainer(mStubViewGroup);
+
+        // Create the ads request.
+        AdsRequest request = mSdkFactory.createAdsRequest();
+        request.setAdTagUrl(adRequestUrl);
+        request.setAdDisplayContainer(mAdDisplayContainer);
+
+        // Request the ad. After the ad is loaded, onAdsManagerLoaded() will be called.
+        mAdsLoader.requestAds(request);
+    }
+
+    /**
+     * Releases related resources.
+     */
+    public void release() {
+        if (mAdsManager != null) {
+            mAdsManager.destroy();
+            mAdsManager = null;
+        }
+        if (mAdsLoader != null) {
+            mAdsLoader.removeAdsLoadedListener(this);
+            mAdsLoader.removeAdErrorListener(this);
+        }
+    }
+
+    @Override
+    public void onAdError(AdErrorEvent adErrorEvent) {
+        Log.e(TAG, adErrorEvent.getError().getMessage());
+        mAdControllerCallback.onAdError();
+        release();
+    }
+
+    /**
+     * An event raised when ads are successfully loaded from the ad server via an AdsLoader.
+     */
+    @Override
+    public void onAdsManagerLoaded(AdsManagerLoadedEvent adsManagerLoadedEvent) {
+        // Ads were successfully loaded, so get the AdsManager instance. AdsManager has
+        // events for ad playback and errors.
+        mAdsManager = adsManagerLoadedEvent.getAdsManager();
+
+        // Attach event and error event listeners.
+        mAdsManager.addAdErrorListener(this);
+        mAdsManager.addAdEventListener(this);
+        mAdsManager.init();
+    }
+
+    /**
+     * Responds to AdEvents.
+     */
+    @Override
+    public void onAdEvent(AdEvent adEvent) {
+        if (DEBUG) {
+            Log.d(TAG, "Event: " + adEvent.getType());
+        }
+        // These are the suggested event types to handle. For full list of all ad event types,
+        // see the documentation for AdEvent.AdEventType.
+        switch (adEvent.getType()) {
+            case LOADED:
+                // AdEventType.LOADED will be fired when ads are ready to be played.
+                // AdsManager.start() begins ad playback. This method is ignored for VMAP or ad
+                // rules playlists, as the SDK will automatically start executing the playlist.
+                mAdsManager.start();
+                break;
+            case COMPLETED:
+                // AdEventType.COMPLETED is fire when ad is completed, but before the fire of
+                // AdEventType.CONTENT_RESUME_REQUESTED.
+                mAdControllerCallback.onAdCompleted();
+                release();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private class VideoAdPlayerImpl implements VideoAdPlayer, VideoPlayer.PlayerCallback {
+        String mAdVideoUrl;
+        VideoPlayer mVideoPlayer;
+        List<VideoAdPlayerCallback> mAdCallbacks;
+
+        VideoAdPlayerImpl() {
+            mAdCallbacks = new ArrayList<>(1);
+        }
+
+        @Override
+        public void loadAd(String adVideoUrl) {
+            mAdVideoUrl = adVideoUrl;
+        }
+
+        @Override
+        public void playAd() {
+            mVideoPlayer = mAdControllerCallback.onAdReadyToPlay(mAdVideoUrl);
+            mVideoPlayer.addPlayerCallback(this);
+            mVideoPlayer.play();
+        }
+
+        @Override
+        public void stopAd() {
+            // Do nothing.
+        }
+
+        @Override
+        public void pauseAd() {
+            // Do nothing.
+        }
+
+        @Override
+        public void resumeAd() {
+            // Do nothing.
+        }
+
+        @Override
+        public void addCallback(VideoAdPlayer.VideoAdPlayerCallback videoAdPlayerCallback) {
+            mAdCallbacks.add(videoAdPlayerCallback);
+        }
+
+        @Override
+        public void removeCallback(VideoAdPlayer.VideoAdPlayerCallback videoAdPlayerCallback) {
+            mAdCallbacks.remove(videoAdPlayerCallback);
+        }
+
+        @Override
+        public VideoProgressUpdate getAdProgress() {
+            if (mVideoPlayer == null) {
+                return VideoProgressUpdate.VIDEO_TIME_NOT_READY;
+            }
+            return new VideoProgressUpdate(
+                    mVideoPlayer.getCurrentPosition(), mVideoPlayer.getDuration());
+        }
+
+        @Override
+        public void onPlay() {
+            for (VideoAdPlayerCallback callback : mAdCallbacks) {
+                callback.onPlay();
+            }
+        }
+
+        @Override
+        public void onCompleted() {
+            for (VideoAdPlayerCallback callback : mAdCallbacks) {
+                callback.onEnded();
+            }
+        }
+
+        @Override
+        public void onError() {
+            for (VideoAdPlayerCallback callback : mAdCallbacks) {
+                callback.onError();
+            }
+        }
+
+        @Override
+        public void onPause() {
+            for (VideoAdPlayerCallback callback : mAdCallbacks) {
+                callback.onPause();
+            }
+        }
+
+        @Override
+        public void onResume() {
+            for (VideoAdPlayerCallback callback : mAdCallbacks) {
+                callback.onResume();
+            }
+        }
+    }
+}
